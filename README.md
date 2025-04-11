d6664230492a651bdc324b9f8d81db9cb65d5a3f
Iulian Peca <pkiulian@gmail.com>
2025-04-11 12:39:23 +0200

    Update version, documentaiton and enable testing

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ The code is developed using:
 - Linux Ubuntu 24.04.2 LTS
 - Spring Boot 3.4.4 
 - Apache Maven 3.9.9 
+- maven-3.9.9 
 - Agnostic of IDE (I used eclipse Version: 2024-09 (4.33.0) Build id: 20240905-0614)
+- Postman (to check the rest API) 
 
 The testing is done using Junit5 and test, building is with maven.  
 
@@ -15,7 +17,7 @@ The testing is done using Junit5 and test, building is with maven.
 Normally, the development can be done with the H2 in-memory database. 
 However, to make the development/test set-up closer to the real world usage, I replaced the H2 with docker test contianer. 
 
-To be able to run the unit testing, please have docker install on the vm: 
+**To be able to run the unit testing, please have docker install on the vm:** 
 
 ```bash
 > docker version
@@ -37,7 +39,85 @@ Steps to run and play with the application:
 ```
 
 ```bash
+#navigate to the right location
+> cd APIAprilFiniance2025/exchange/
+```
+
+```bash
 # build the application
 > mvn clean install 
 ```
+
+```bash
+# shuts down and cleans up everything started by docker-compose up
+> docker-compose down --volumes
+```
+
+```bash
+# check and make sure there is no other docker container running
+> docker ps
+```
+
+```bash
+# build images if needed and run (in background) containers definied in docker-compose.yml
+> docker-compose up -d --build
+```
+
+Now the application is ready for manual testing.
+
+Open Postman and insert two accounts: 
+
+```bash
+POST: http://localhost:8080/api/finance/createAccount
+Body raw: 
+{
+  "ownerId": 1,
+  "currency": "EUR",
+  "balance": 1000.0
+}
+```
+```bash
+POST: http://localhost:8080/api/finance/createAccount
+Body raw: 
+{
+  "ownerId": 2,
+  "currency": "EUR",
+  "balance": 2000.0
+}
+```
+Received Code for the above should be 201 (Created)
+
+Using Postman execute one transfer: 
+```bash
+POST: http://localhost:8080/api/finance/transfer
+Body:
+{
+  "debitAccountId": 1,
+  "creditAccountId": 2,
+  "amount": 155.0,
+  "debitCurrency": "EUR",
+  "creditCurrency": "EUR"
+}
+```
+
+Now Check on browser: http://localhost:8080/api/finance/account/1 it should look like this: 
+```bash
+accountId	1
+ownerId	1
+currency	"EUR"
+balance	845.0JS:845
+version	1
+```
+and http://localhost:8080/api/finance/account/2:
+```bash
+accountId	2
+ownerId	2
+currency	"EUR"
+balance	2155.0JS:2155
+version	1
+```
+
+
+
+
 
--- exchange/src/test/java/org/exchage/exchange/service/AccountServiceStressTest.java
+++ exchange/src/test/java/org/exchage/exchange/service/AccountServiceStressTest.java
@@ -1,7 +1,19 @@
 package org.exchage.exchange.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.exchage.exchange.account.Account;
+import org.exchage.exchange.repository.AccountRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -16,152 +28,135 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class AccountServiceStressTest {
 
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
-            .withDatabaseName("exchange_db")
-            .withUsername("test")
-            .withPassword("test")
-            .waitingFor(Wait.forListeningPort())  // Ensure PostgreSQL is ready
-            .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1));  // Log message for readiness
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16").withDatabaseName("exchange_db")
+			.withUsername("test").withPassword("test").waitingFor(Wait.forListeningPort()) // Ensure PostgreSQL is ready
+			.waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1)); // Log message
+																											// for
+																											// readiness
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        // This will inject the dynamically assigned JDBC URL from the container
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", () -> postgres.getUsername());
-        registry.add("spring.datasource.password", () -> postgres.getPassword());
-        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-    }
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		// This will inject the dynamically assigned JDBC URL from the container
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", () -> postgres.getUsername());
+		registry.add("spring.datasource.password", () -> postgres.getPassword());
+		registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+	}
 
-    @Test
-    public void testConnection() {
-        // Add a simple assertion to verify that Spring Boot is connecting to the database
-        Assertions.assertNotNull(postgres.getJdbcUrl(), "PostgreSQL URL is not set correctly");
-    }
+	@Test
+	public void testConnection() {
+		// Add a simple assertion to verify that Spring Boot is connecting to the
+		// database
+		Assertions.assertNotNull(postgres.getJdbcUrl(), "PostgreSQL URL is not set correctly");
+	}
+
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private AccountRepository accountRepository;
+
+	private Long debitAccountId;
+	private Long creditAccountId;
+
+	@BeforeEach
+	public void setUp() {
+		// Initialize two test accounts
+		Account debitAccount = new Account();
+		debitAccount.setOwnerId(1L);
+		debitAccount.setCurrency("EUR");
+		debitAccount.setBalance(1000.0);
+		debitAccount = this.accountRepository.save(debitAccount);
+
+		Account creditAccount = new Account();
+		creditAccount.setOwnerId(2L);
+		creditAccount.setCurrency("EUR");
+		creditAccount.setBalance(0.0);
+		creditAccount = this.accountRepository.save(creditAccount);
+
+		this.debitAccountId = debitAccount.getAccountId();
+		this.creditAccountId = creditAccount.getAccountId();
+	}
 
 
+	@Test
+	public void testOptimisticLockingConcurrentTransfers() throws InterruptedException, ExecutionException {
 
-//	@DynamicPropertySource
-//	static void configureProperties(DynamicPropertyRegistry registry) {
-//
-//		System.out.println(">>>>>>>>>>>>>> PostgreSQL URL: " + postgres.getJdbcUrl());
-//
-//		registry.add("spring.datasource.url", postgres::getJdbcUrl);
-//		registry.add("spring.datasource.username", postgres::getUsername);
-//		registry.add("spring.datasource.password", postgres::getPassword);
-//		registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
-//		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-//	}
+		int successCount = 0;
+		int failureCount = 0;
+		int numberOfThreads = 10;
+		ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+		List<Callable<Boolean>> tasks = new ArrayList<>();
+		for (int i = 0; i < numberOfThreads; i++) {
+			tasks.add(() -> {
+				try {
+					return this.accountService.transferFunds(this.debitAccountId, this.creditAccountId, 10.0, "EUR",
+							"EUR");
+				} catch (Exception e) {
+					return false;
+				}
+			});
+		}
 
-	// @Autowired
-	// private AccountService accountService;
-	// @Autowired
-	// private AccountRepository accountRepository;
+		// Execute all tasks concurrently
+		List<Future<Boolean>> results = executorService.invokeAll(tasks);
 
-	// private Long debitAccountId;
-	// private Long creditAccountId;
+		for (Future<Boolean> result : results) {
+			if (result.get()) {
+				successCount++;
+			} else {
+				failureCount++;
+			}
+		}
 
-	// @BeforeEach
-	// public void setUp() {
-	// 	// Initialize two test accounts
-	// 	Account debitAccount = new Account();
-	// 	debitAccount.setOwnerId(1L);
-	// 	debitAccount.setCurrency("EUR");
-	// 	debitAccount.setBalance(1000.0);
-	// 	debitAccount = this.accountRepository.save(debitAccount);
+		Assertions.assertEquals(1, successCount);
+		Assertions.assertEquals(numberOfThreads - 1, failureCount);
 
-	// 	Account creditAccount = new Account();
-	// 	creditAccount.setOwnerId(2L);
-	// 	creditAccount.setCurrency("EUR");
-	// 	creditAccount.setBalance(0.0);
-	// 	creditAccount = this.accountRepository.save(creditAccount);
+		executorService.shutdown();
+	}
 
-	// 	this.debitAccountId = debitAccount.getAccountId();
-	// 	this.creditAccountId = creditAccount.getAccountId();
-	// }
+	@Test
+	public void testRetryLogicConcurrentTransfers() throws InterruptedException, ExecutionException {
 
-	// @Test
-	// @Disabled
-	// public void testOptimisticLockingConcurrentTransfers() throws InterruptedException, ExecutionException {
+		int numberOfThreads = 10;
+		ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
 
-	// 	int successCount = 0;
-	// 	int failureCount = 0;
-	// 	int numberOfThreads = 10;
-	// 	ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+		List<Callable<Boolean>> tasks = new ArrayList<>();
+		for (int i = 0; i < numberOfThreads; i++) {
+			tasks.add(() -> {
+				try {
+					return this.accountService.safeTransfer(this.debitAccountId, this.creditAccountId, 10.0, "EUR",
+							"EUR");
+				} catch (Exception e) {
+					e.printStackTrace();
+					return false;
+				}
+			});
+		}
 
-	// 	List<Callable<Boolean>> tasks = new ArrayList<>();
-	// 	for (int i = 0; i < numberOfThreads; i++) {
-	// 		tasks.add(() -> {
-	// 			try {
-	// 				return this.accountService.transferFunds(this.debitAccountId, this.creditAccountId, 10.0, "EUR",
-	// 						"EUR");
-	// 			} catch (Exception e) {
-	// 				return false;
-	// 			}
-	// 		});
-	// 	}
+		// Execute all tasks concurrently
+		List<Future<Boolean>> results = executorService.invokeAll(tasks);
 
-	// 	// Execute all tasks concurrently
-	// 	List<Future<Boolean>> results = executorService.invokeAll(tasks);
+		boolean allPass = true;
+		for (Future<Boolean> result : results) {
+			if (!result.get()) {
+				allPass = false;
+				break;
+			}
+		}
 
-	// 	for (Future<Boolean> result : results) {
-	// 		if (result.get()) {
-	// 			successCount++;
-	// 		} else {
-	// 			failureCount++;
-	// 		}
-	// 	}
+		Assertions.assertTrue(allPass);
 
-	// 	Assertions.assertEquals(1, successCount);
-	// 	Assertions.assertEquals(numberOfThreads - 1, failureCount);
+		Account creditAccount = this.accountRepository.findById(this.creditAccountId).orElseThrow();
+		Account debitAccount = this.accountRepository.findById(this.debitAccountId).orElseThrow();
 
-	// 	executorService.shutdown();
-	// }
+		// The debit account balance should be reduced by 10 * numberOfThreads
+		double expectedDebitBalance = 1000.0 - (10.0 * numberOfThreads);
+		double expectedCreditBalance = 10.0 * numberOfThreads;
+		Assertions.assertEquals(expectedDebitBalance, debitAccount.getBalance());
+		Assertions.assertEquals(expectedCreditBalance, creditAccount.getBalance());
 
-	// @Test
-	// public void testRetryLogicConcurrentTransfers() throws InterruptedException, ExecutionException {
-
-	// 	int numberOfThreads = 10;
-	// 	ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
-
-	// 	List<Callable<Boolean>> tasks = new ArrayList<>();
-	// 	for (int i = 0; i < numberOfThreads; i++) {
-	// 		tasks.add(() -> {
-	// 			try {
-	// 				return this.accountService.safeTransfer(this.debitAccountId, this.creditAccountId, 10.0, "EUR",
-	// 						"EUR");
-	// 			} catch (Exception e) {
-	// 				e.printStackTrace();
-	// 				return false;
-	// 			}
-	// 		});
-	// 	}
-
-	// 	// Execute all tasks concurrently
-	// 	List<Future<Boolean>> results = executorService.invokeAll(tasks);
-
-	// 	boolean allPass = true;
-	// 	for (Future<Boolean> result : results) {
-	// 		if (!result.get()) {
-	// 			allPass = false;
-	// 			break;
-	// 		}
-	// 	}
-
-	// 	Assertions.assertTrue(allPass);
-
-	// 	Account creditAccount = this.accountRepository.findById(this.creditAccountId).orElseThrow();
-	// 	Account debitAccount = this.accountRepository.findById(this.debitAccountId).orElseThrow();
-
-	// 	// The debit account balance should be reduced by 10 * numberOfThreads
-	// 	double expectedDebitBalance = 1000.0 - (10.0 * numberOfThreads);
-	// 	double expectedCreditBalance = 10.0 * numberOfThreads;
-
-	// 	Assertions.assertEquals(expectedDebitBalance, debitAccount.getBalance());
-	// 	Assertions.assertEquals(expectedCreditBalance, creditAccount.getBalance());
-
-	// 	executorService.shutdown();
-	// }
-
+		executorService.shutdown();
+	}
 }
